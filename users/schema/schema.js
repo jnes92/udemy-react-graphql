55a641bf7ac92e3248c1a0acb0cb66094dd71d7c
test <jonas.oesterle@gmx.de>
2018-06-02 13:11:51 +0200

    [10] Adding the RootQuery

--- users/schema/schema.js
+++ users/schema/schema.js
@@ -8,8 +8,21 @@ const {
 const UserType = new GraphQLObjectType({
     name: 'User',
     fields: {
-        id: { type: GraphQLString } ,
-        firstName: { type: GraphQLString } ,
-        age: { type: GraphQLInt } 
+        id: { type: GraphQLString },
+        firstName: { type: GraphQLString },
+        age: { type: GraphQLInt }
     }
 });
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        user: {
+            type: UserType,
+            args: { id: { type: GraphQLString } },
+            resolve(parentValue, args) {
+                
+            }
+        }
+    }
+})
