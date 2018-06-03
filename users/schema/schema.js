8febf643a42bac475a1dddf51e6cb51c205cf9f4
test <jonas.oesterle@gmx.de>
2018-06-03 10:48:28 +0200

    [28] Mutation for editing a user

--- users/schema/schema.js
+++ users/schema/schema.js
@@ -83,7 +83,19 @@ const mutation = new GraphQLObjectType({
             resolve(parentValue, { id }) {
                 return axios.delete(`${BaseURL}users/${id}`).then(resp => resp.data);
             } 
-        }
+        },
+        editUser: {
+            type: UserType,
+            args: {
+                id: { type: new GraphQLNonNull(GraphQLString) }, 
+                firstName: { type: GraphQLString },
+                age: { type: GraphQLInt },
+                companyId: { type: GraphQLString }
+            },
+            resolve(parentValue, args) {
+                return axios.patch(`${BaseURL}users/${args.id}`, args).then(resp => resp.data);
+            } 
+        },
     }
 })
 
