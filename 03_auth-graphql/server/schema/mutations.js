556dead1d3f73dccea5061e152f0fa9dafc0d518
test <jonas.oesterle@gmx.de>
2018-07-15 10:36:40 +0200

    [84] Adding logout mutation

--- 03_auth-graphql/server/schema/mutations.js
+++ 03_auth-graphql/server/schema/mutations.js
@@ -22,7 +22,16 @@ const mutation = new GraphQLObjectType({
                     req
                 });
             } 
-        }
+        },
+        logout: {
+            type: UserType,
+            resolve(parentValue, args, req) {
+                const { user } = req;
+                req.logout();
+                return user;
+            } 
+        },
+
     }
 });
 
