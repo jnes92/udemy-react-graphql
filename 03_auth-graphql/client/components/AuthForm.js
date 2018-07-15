560235d9e06940b6893abd582b59eb78e6299325
test <jonas.oesterle@gmx.de>
2018-07-15 17:04:05 +0200

    [98] Submitting the Auth Form

--- 03_auth-graphql/client/components/AuthForm.js
+++ 03_auth-graphql/client/components/AuthForm.js
@@ -9,26 +9,33 @@ class AuthForm extends Component {
             password: ''
         }
     }
+
+    onSubmit(event){
+        event.preventDefault();
+
+        this.props.onSubmit(this.state);
+    }
+
     render() {
         return (
             <div className="row">
-                <form className="col s6">
+                <form className="col s6" onSubmit={this.onSubmit.bind(this)}>
                     <div className="input-field">
-                        <label>Email</label>
                         <input 
+                            placeholder="Email"
                             value={this.state.email}
                             onChange={e => this.setState({email: e.target.value})}
                         />
                     </div>
                     <div className="input-field">
-                        <label>Password</label>
                         <input 
+                            placeholder="Password"
                             type="password"
                             value={this.state.password}
                             onChange={e => this.setState({password: e.target.value})}
                         />
                     </div>
-                    <button className="btn btn-submit">Submit</button>
+                    <button type="submit" className="btn btn-submit">Submit</button>
                 </form>
             </div>
         );
