d1eff4bf6c2c3fae397ac826889d8fe4b9b413fa
test <jonas.oesterle@gmx.de>
2018-07-15 15:01:33 +0200

    [92] Login and Signup Buttons

--- 03_auth-graphql/client/components/Header.js
+++ 03_auth-graphql/client/components/Header.js
@@ -1,25 +1,36 @@
 import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
 import query from '../queries/CurrentUser';
+import { Link } from 'react-router';
 
 class Header extends Component {
     renderButtons() {
         let { loading, user } = this.props.data;
-        console.log(this.props.data);
         if (loading) return null;
 
         if (user)
             return <div> Logout </div>;
 
-        else
-            return <div> You´re not signed in. </div>;
+        else return (
+            <div>
+                <li>
+                    <Link to="/signup" > Signup </Link>
+                </li>
+                <li>
+                    <Link to="/login" > Login </Link>
+                </li>
+            </div>
+        );
     }
 
     render() {
         return (
             <nav>
                 <div className="nav-wraper">
-                    {this.renderButtons()}
+                    <Link to="/" className="brand-logo left"> Home </Link>
+                    <ul className="right">
+                        {this.renderButtons()}
+                    </ul>
                 </div>
             </nav>
         );
